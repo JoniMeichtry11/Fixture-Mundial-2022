d64fe267a842881dd65cf331eeefb93a3dff5332
JoniMeichtry11 <joni.meichtry@gmail.com>
2021-09-25 22:15:51 -0300

    commit fixture

---
+++ js/main.js
@@ -0,0 +1,7 @@
+window.onload = function (){
+    let logo2 = document.getElementById('logo2');
+    let logo = document.getElementById('logo');
+
+    logo.style.opacity = 100;
+    logo2.style.opacity = 100;
+}
